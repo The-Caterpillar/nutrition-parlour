574adbdc65190681add3d2fa6062d869f0bc1318
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2025-11-03 01:47:30 +0530

    created different component for button

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,11 +1,14 @@
+import Button from "./Button";
+
 const Modal = ({rating, closeModal}) => {
     return ( <>
         <div className="modal-overlay">
             <div className="modal">
                 <h2>Thank You!!</h2>
                 <p>You rated us {rating} star{rating == 1 ? 's' : ''}</p>
-                <button className="close-btn" onClick={closeModal}>
-                    Close </button>
+                <Button className="close-btn" onClick={closeModal}>
+                    Close
+                </Button>
             </div>
         </div>
     </> );
