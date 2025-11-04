3a59e9dc42b98cc12c8bcf160edb7d9f9685996b
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2025-11-05 04:14:03 +0530

    A basic structure for notes app. - Create new notes, view those notes, etc. - Required changes:     1. The notes should stay even after refersh.     2. Option to delete one/+all notes

---
+++ src/Components/NoteList.jsx
@@ -0,0 +1,32 @@
+import Notes from "./Notes";
+
+const NoteList = ({notes}) => {
+
+    const defaultMessage = (<p className="text-center text-gray-500">No notes yet! </p>);
+
+    return ( notes.length == 0 ? defaultMessage :
+        <>
+            <div className="space-y-4">
+                {
+                    notes.map((note) => (
+                        <div key={note.id} className="p-4 bg-white rounded-lg shadow-md border-l-4">
+                            <h3 className="text-lg font-bold">
+                                {note.title}
+                            </h3>
+                            <p className="text-sm text-gray-600">
+                                <strong> Category: </strong> {note.category}
+                            </p>
+                            <p className="text-sm text-gray-600">
+                                <strong> Priority: </strong> {note.priority}
+                            </p>
+                            <p className="text-sm text-gray-600">
+                                <strong> Description: </strong> {note.description}
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
+        </> );
+}
+
+export default NoteList;
