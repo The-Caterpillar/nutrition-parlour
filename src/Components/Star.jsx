5c46a5d3af20364965edd99d968b5b8866838e6a
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2025-11-07 00:00:17 +0530

    remove unused code

--- src/Components/Star.jsx
+++ src/Components/Star.jsx
@@ -4,7 +4,6 @@ const Star = ({star, rating, hover, color, ratingClick, hoverEnter, hoverLeave})
             onClick={() => ratingClick(star)}
             onMouseEnter={() => hoverEnter(star)}
             onMouseLeave={() => hoverLeave(0)}
-            // className='star'
             style={{color: star<=(hover || rating) ? color : '#ccc'}}
             className='star'>
                 {'\u2605'}</span>
